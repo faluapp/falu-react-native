84a721ea253c59ae1820b2ef8145b20dde3e08db
Seth Onyango <sethi.onyango@gmail.com>
2024-06-07 08:43:14 +0300

    Replace vitest with jest

--- packages/identity/src/__tests__/index.test.tsx
+++ packages/identity/src/__tests__/index.test.tsx
@@ -1,7 +1,1 @@
-import { expect, test } from 'vitest';
-
-// checkout https://github.com/sheremet-va/vitest-react-native/blob/master/example/index.test.jsx
-
-test('sample', async () => {
-  expect(1 + 1).toBe(2);
-});
+it.todo('write a test');
