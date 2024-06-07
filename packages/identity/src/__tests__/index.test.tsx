7d054e9d2101a79566a9e0de06b86445a850285d
Seth Onyango <sethi.onyango@gmail.com>
2024-06-07 09:57:03 +0300

    Disable test task

--- packages/identity/src/__tests__/index.test.tsx
+++ packages/identity/src/__tests__/index.test.tsx
@@ -1,1 +1,7 @@
-it.todo('write a test');
+import { describe, expect, test } from '@jest/globals';
+
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
