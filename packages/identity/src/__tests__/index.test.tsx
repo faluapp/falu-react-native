741ae24c05e79a3f5b81e77fb1648559ce9337e8
Seth Onyango <57979366+sethonyango@users.noreply.github.com>
2024-06-10 04:40:31 +0300

    Replace vitest with jest (#80)
    * Replace vitest with jest
    
    * Disable test task
    
    * Move babel config to identity package and update jest configuration
    
    * Fix code formatting issue
    
    * Fix missing new lines
    
    ---------
    
    Co-authored-by: Seth Onyango <57979366+SecyWecy@users.noreply.github.com>

--- packages/identity/src/__tests__/index.test.tsx
+++ packages/identity/src/__tests__/index.test.tsx
@@ -1,7 +1,7 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from '@jest/globals';
 
-// checkout https://github.com/sheremet-va/vitest-react-native/blob/master/example/index.test.jsx
-
-test('sample', async () => {
-  expect(1 + 1).toBe(2);
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
